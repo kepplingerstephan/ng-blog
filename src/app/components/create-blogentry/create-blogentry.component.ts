3f3d4848a3e089bc738d5d562c8e00cbd0249ad2
kepplingerstephan <stephan.kepplinger@gmail.com>
2023-04-13 23:19:43 +0200

    Fixed tinymce not working

--- src/app/components/create-blogentry/create-blogentry.component.ts
+++ src/app/components/create-blogentry/create-blogentry.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit} from '@angular/core';
 
+declare var tinymce: any; // Declare the tinymce global object
 
 @Component({
   selector: 'app-create-blogentry',
   templateUrl: './create-blogentry.component.html',
   styleUrls: ['./create-blogentry.component.css'],
 })
-export class CreateBlogentryComponent {
-  content : string= '';
-  
-  handleEvent($content:any){
-    console.log($content.getContent());
+export class CreateBlogentryComponent implements AfterViewInit{
+  @ViewChild('myEditor') myEditor :any;
+
+  ngAfterViewInit() {
+    // Ensure that the TinyMCE editor has been fully initialized
+    if (this.myEditor && this.myEditor.editor) {
+      console.log('TinyMCE editor initialized');
+    }
+  }
+  logContent() {
+    const content = tinymce.get('myEditor').getContent();
+    console.log(content);
+    
+  }
+  submit(){
+    return 0;
   }
 
-  logContent(){
-    
-    // var i = tinymce.getContent();
-    console.log("");
-  }
 }
