1c09538a705228e4d1ad254032a5f7ab7ed8e9cd
kepplingerstephan <stephan.kepplinger@gmail.com>
2023-04-13 19:36:07 +0200

    add tinymce

--- src/app/components/create-blogentry/create-blogentry.component.ts
+++ src/app/components/create-blogentry/create-blogentry.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-create-blogentry',
   templateUrl: './create-blogentry.component.html',
-  styleUrls: ['./create-blogentry.component.css']
+  styleUrls: ['./create-blogentry.component.css'],
 })
 export class CreateBlogentryComponent {
+  content : string= '';
+  
+  handleEvent($content:any){
+    console.log($content.getContent());
+  }
 
+  logContent(){
+    
+    // var i = tinymce.getContent();
+    console.log("");
+  }
 }
