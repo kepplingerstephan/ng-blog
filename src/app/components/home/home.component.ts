0b56eff6ab80e04daf2887f171a0668a6eb5120b
kepplingerstephan <stephan.kepplinger@gmail.com>
2023-04-16 20:18:23 +0200

    trying to fix user

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/models/User';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+@Injectable()
+export class HomeComponent implements OnInit, OnDestroy, OnChanges {
+  public user: User | null = null;
+  private userSubscription: Subscription | undefined;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userSubscription = this.userService.user$.subscribe(user => {
+      this.user = user;
+      console.log(this.user?.name);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['user'] && changes['user'].currentValue) {
+      this.user = changes['user'].currentValue;
+      console.log(this.user?.name);
+    }
+  }
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,32 +1,51 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Injectable, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/models/User';
 import { APIResponse } from 'src/models/APIResponse';
 import { UserService } from 'src/app/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 
 @Injectable()
-export class UserComponent implements OnInit {
-  constructor(private http: HttpClient, public userService: UserService) { }
+export class UserComponent implements OnInit, OnDestroy, AfterViewInit {
+  public user: User | null = null;
+  private userSubscription: Subscription | undefined;
+
+  constructor(private http: HttpClient, private userService: UserService) { }
+
+  public selectedUser : string = "";
 
   public users: User[] = [];
-  // public selectedUser : string = "";
 
   async ngOnInit() {
     this.users = (await getUsers()).result;
+    this.userSubscription = this.userService.user$.subscribe(user => {
+      this.user = user;
+      // console.log(this.user?.name); // Log inside the subscription callback
+    });
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.user?.name); // Log user name after view has been initialized
+  }
+
+  ngOnDestroy(): void {
+    if(this.userSubscription){
+      this.userSubscription.unsubscribe();
+    }
   }
 
   selectChanged(){
-    // var user = this.users.find(u => u.id === parseInt(this.selectedUser))
-    // if(user && this.userService.user){
-    //     this.userService.user = user;
-    // }
-    console.log(this.userService.user?.name);
+    if(this.selectedUser){
+      const u = this.users.find(u => u.id === parseInt(this.selectedUser)) || null;
+      this.userService.setUser(u);
+      console.log(this.user?.name);
+    }
   }
 }
 
