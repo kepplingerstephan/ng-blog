3e0e55b3b96e3ff2aa41e3430a4dff02ef3831b8
kepplingerstephan <stephan.kepplinger@gmail.com>
2023-04-16 17:46:58 +0200

    ,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import {HttpClientModule} from '@angular/common/http';
     CreateBlogentryComponent,
     HomeComponent,
     NavbarComponent,
-    UserComponent
+    UserComponent,
   ],
   imports: [
     BrowserModule,
